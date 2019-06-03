31b8a0be5c27db099208ca16bde47983df3e70b4
Suresh Rao <37570854+raosuresh94@users.noreply.github.com>
2019-06-03 12:54:43 +0530

    Update Init.php

--- wpdk-lib/Init.php
+++ wpdk-lib/Init.php
@@ -1,6 +1,9 @@
 <?php
 // include Enqueue Class
-require_once get_template_directory().'/Enqueue.php';
+require_once get_template_directory().'/wpdk-lib/Enqueue.php';
 
 // Include PostType Generator Class
-require_once get_template_directory().'/PostTypeGenerator.php';
+require_once get_template_directory().'/wpdk-lib/PostTypeGenerator.php';
+
+// Include Customize Register
+require_once get_template_directory().'/wpdk-lib/Customizer.php';
